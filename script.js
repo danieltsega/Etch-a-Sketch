aa7c34feb39b34f60e6add7157d05cdd312425c1
danieltsega <danieltsega6658@gmail.com>
2024-08-11 00:21:58 +0300

    feat: add event listner for mouse over to make the background color of the boxes to black that makes a drawing

--- script.js
+++ script.js
@@ -1,16 +1,12 @@
 let container = document.querySelector(".container");
 container.classList = "flex border-2 border-gray-400 m-2 flex-wrap w-80 h-80";
 
-//let box2 = document.createElement("div");
-//box2.classList = "w-52 h-52 bg-yellow-600";
-
-//let box3 = document.createElement("div");
-//box3.classList = "w-52 h-52 bg-red-600";
-
 for (let i = 1; i <= 16 * 16; i++) {
   const box1 = document.createElement("div");
   box1.classList = "flexBox";
+
   container.appendChild(box1);
+  box1.addEventListener("mouseover", function () {
+    box1.classList.add("bg-black");
+  });
 }
-//container.appendChild(box2);
-//container.appendChild(box3);
